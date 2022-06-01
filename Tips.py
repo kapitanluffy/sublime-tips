4ec5d162635282891f440dc66666bdba7104d661
kapitanluffy <kapitanluffy@gmail.com>
2022-06-02 03:09:34 +0800

    Add cross platform support

--- Tips.py
+++ Tips.py
@@ -1,34 +1,35 @@
-from os import path
+from os import  path
 import sublime
 import sublime_plugin
 import random
 import re
 
 def plugin_loaded():
-    file = path.join(sublime.packages_path(), "Tips", "tips.txt");
+    platform = sublime.platform()
+
+    if platform == "windows":
+        tips = "tips (Windows).txt"
+
+    if platform == "linux":
+        tips = "tips (Linux).txt"
+
+    if platform == "osx":
+        tips = "tips (OSX).txt"
+
+    file = path.join(sublime.packages_path(), "Tips", tips);
     fh = open(file);
     Tips.tips = fh.readlines()
+    fh.close()
 
 class Tips(sublime_plugin.EventListener):
     tips = []
 
     def on_activated_async(self, view):
+        if len(self.tips) <= 0:
+            return
+
         random.shuffle(self.tips)
         tip = self.tips[0].strip()
-
-        platform = sublime.platform()
-        primaryKey = "Ctrl"
-        superKey = "Win"
-        altKey = "Alt"
-
-        if platform == "osx":
-            primaryKey = "Cmd"
-            superKey = "Cmd"
-            altKey = "Opt"
-
-        tip = re.sub(r'\[PRIMARY\]', primaryKey, tip)
-        tip = re.sub(r'\[SUPER\]', superKey, tip)
-        tip = re.sub(r'\[ALT\]', altKey, tip)
 
         sublime.status_message("💡 {tip}".format(tip=tip))
 
