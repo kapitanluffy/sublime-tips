0ca5b0cd512f50c9bb2494e974ee807cd7e9a71f
kapitanluffy <kapitanluffy@gmail.com>
2023-04-23 18:17:35 +0800

    Show tips on start

--- Tips.py
+++ Tips.py
@@ -3,6 +3,7 @@ import sublime
 import sublime_plugin
 import random
 import re
+from typing import List
 
 def plugin_loaded():
     platform = sublime.platform()
@@ -18,6 +19,35 @@ def plugin_loaded():
 
     tips = sublime.load_resource(path.join("Packages", "Tips", tips))
     Tips.tips = tips.splitlines()
+
+    sublime.set_timeout_async(autostart)
+
+def autostart():
+    settings = sublime.load_settings('Tips.sublime-settings')
+
+    if settings["show_random_tips_on_start"] is False:
+        return
+
+    cache: List[str] = [random.choice(Tips.tips).strip()]
+    index: int = 0
+
+    while True:
+        msg = cache[index]
+        msg = msg.replace(': ', ':\n')
+
+        result: sublime.DialogResult = sublime.yes_no_cancel_dialog(
+            msg=msg, yes_title='Next Tip', no_title='Previous Tip', title='Random Tip 💡'
+        )
+
+        if result == sublime.DialogResult.CANCEL:
+            break
+
+        if result == sublime.DialogResult.YES:
+            cache.append(random.choice(Tips.tips).strip())
+            index += 1
+        elif index > 0:
+            index -= 1
+
 
 class Tips(sublime_plugin.EventListener):
     tips = []
