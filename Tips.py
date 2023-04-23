74c3e1c7d510f5328d0371e790b48cbc5a260f2d
kapitanluffy <kapitanluffy@gmail.com>
2023-04-24 01:04:51 +0800

    Apply updates
    - Display tips in the status bar longer
    - Add "Show Random Tip" command

--- Tips.py
+++ Tips.py
@@ -49,6 +49,10 @@ def autostart():
             index -= 1
 
 
+def clear_status_tip(view: sublime.View):
+    view.erase_status("tips_status_bar_msg")
+
+
 class Tips(sublime_plugin.EventListener):
     tips = []
 
@@ -59,5 +63,24 @@ class Tips(sublime_plugin.EventListener):
         random.shuffle(self.tips)
         tip = self.tips[0].strip()
 
-        sublime.status_message("💡 {tip}".format(tip=tip))
+        settings = sublime.load_settings('Tips.sublime-settings')
+        msgFormat = str(settings.get("message_format", "💡 {tip}"))
+        hideDelay = settings.get("display_seconds", 60)
+        hideDelay = int(str(hideDelay)) * 1000
 
+        view.set_status("tips_status_bar_msg", msgFormat.format(tip=tip))
+
+        if hideDelay > 0:
+            sublime.set_timeout_async(lambda v=view: clear_status_tip(v), hideDelay)
+
+
+class TipsToggleAutostartCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        settings = sublime.load_settings('Tips.sublime-settings')
+        settings.set("show_random_tips_on_start", not settings["show_random_tips_on_start"]);
+        sublime.save_settings('Tips.sublime-settings')
+
+
+class TipsShowRandomCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        sublime.set_timeout_async(autostart)
